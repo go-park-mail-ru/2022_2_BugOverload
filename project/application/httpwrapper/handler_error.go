4f4f3400bdb22416b805356364a8c72c6176eb3d
Andeo1812 <alexanforall@gmail.com>
2022-09-30 22:33:20 +0300

    TP-0f5 update: tests

--- project/application/httpwrapper/handler_error.go
+++ project/application/httpwrapper/handler_error.go
@@ -7,21 +7,24 @@ import (
 	"go-park-mail-ru/2022_2_BugOverload/project/application/errorshandlers"
 )
 
-//  type ErrResponse struct {
-//  }
+type ErrResponse struct {
+	ErrMassage string `json:"error,omitempty"`
+}
 
 func DefHandlerError(w http.ResponseWriter, err error) {
-	body := `{"error":"` + err.Error() + `"}`
+	errResp := ErrResponse{
+		err.Error(),
+	}
 
 	if errors.Is(err, errorshandlers.ErrUnsupportedMediaType) {
-		Response(w, http.StatusUnsupportedMediaType, body)
+		Response(w, http.StatusUnsupportedMediaType, errResp)
 		return
 	}
 
 	if errors.Is(err, errorshandlers.ErrCookieNotExist) {
-		Response(w, http.StatusForbidden, body)
+		Response(w, http.StatusForbidden, errResp)
 		return
 	}
 
-	Response(w, http.StatusBadRequest, body)
+	Response(w, http.StatusBadRequest, errResp)
 }
