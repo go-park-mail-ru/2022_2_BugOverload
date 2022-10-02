1e9e1e645831ba7eeef4a7216441786a6200fa34
Andeo1812 <alexanforall@gmail.com>
2022-10-03 01:01:34 +0300

    end local tast

--- project/cors/cors.go
+++ project/cors/cors.go
@@ -6,12 +6,15 @@ import (
 	"github.com/rs/cors"
 )
 
-const frontend = "http://localhost:3001"
+const (
+	frontendDebug = "http://localhost:3001"
+	frontendMain  = "http://movie-gate.online"
+)
 
 func NewCorsOptions() *cors.Cors {
 	c := cors.New(cors.Options{
 		AllowedMethods:   []string{http.MethodGet, http.MethodPost},
-		AllowedOrigins:   []string{frontend},
+		AllowedOrigins:   []string{frontendDebug, frontendMain},
 		AllowCredentials: true,
 		AllowedHeaders:   []string{"Content-Type", "Content-length"},
 		Debug:            true,
