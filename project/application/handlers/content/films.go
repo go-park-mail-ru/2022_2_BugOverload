1b7810c58c072d8fbf6e841fd366096b3ef9c017
Andeo1812 <alexanforall@gmail.com>
2022-10-07 12:21:35 +0300

    TP-729 update: tests

--- project/application/handlers/content/films.go
+++ project/application/handlers/content/films.go
@@ -155,7 +155,7 @@ func (hf *HandlerFilms) GetRecommendedFilm(w http.ResponseWriter, r *http.Reques
 	var recommendFilmRequest RecommendFilmRequest
 
 	max := hf.storage.GetStorageLen()
-	min := 0
+	min := max - 3
 
 	if max == 0 {
 		httpwrapper.DefHandlerError(w, errorshandlers.ErrFilmNotFound)
