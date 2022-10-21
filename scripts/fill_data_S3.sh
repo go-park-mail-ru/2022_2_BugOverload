f3f293f07bedddee5560c84ee85ada5e37787301
Andeo1812 <alexanforall@mail.ru>
2022-10-21 23:41:10 +0300

    TP-112 update: scripts

--- scripts/fill_data_S3.sh
+++ scripts/fill_data_S3.sh
@@ -10,18 +10,18 @@ readonly LOCALSTACK_S3_URL=$2
 
 for file in $(find "$1" -type f -name "*"); do
   if [[ "$file" == *"$HOR"* ]]; then
-    aws --endpoint-url=$LOCALSTACK_S3_URL s3 cp "$file" s3://films/posters/hor/
+    aws --endpoint-url="$LOCALSTACK_S3_URL" s3 cp "$file" s3://films/posters/hor/
   fi
 
   if [[ "$file" == *"$VER"* ]]; then
-    aws --endpoint-url=$LOCALSTACK_S3_URL s3 cp "$file" s3://films/posters/ver/
+    aws --endpoint-url="$LOCALSTACK_S3_URL" s3 cp "$file" s3://films/posters/ver/
   fi
 
   if [[ "$file" == *"$DEFAULT"* ]]; then
-    aws --endpoint-url=$LOCALSTACK_S3_URL s3 cp "$file" s3://default/
+    aws --endpoint-url="$LOCALSTACK_S3_URL" s3 cp "$file" s3://default/
   fi
 
   if [[ "$file" == *"$AVATAR"* ]]; then
-      aws --endpoint-url=$LOCALSTACK_S3_URL s3 cp "$file" s3://users/avatar/
-    fi
+    aws --endpoint-url="$LOCALSTACK_S3_URL" s3 cp "$file" s3://users/avatar/
+  fi
 done
