57a2283d44fb101639c9e7deb4d5f48fe85b6d6c
Ivan Biryukov <vanyabiryukov@yandex.ru>
2022-12-01 01:37:22 +0300

    add 20% test cov

---
+++ internal/api/http/delivery/handlers/authsignup_test.go
@@ -0,0 +1,89 @@
+package handlers
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/require"
+
+	"go-park-mail-ru/2022_2_BugOverload/internal/api/http/delivery/models"
+	mockAuthClient "go-park-mail-ru/2022_2_BugOverload/internal/auth/delivery/grpc/client/mocks"
+	modelsGlobal "go-park-mail-ru/2022_2_BugOverload/internal/models"
+)
+
+func TestAuthSignUpHandler_Action_OK(t *testing.T) {
+	t.Parallel()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	authService := mockAuthClient.NewMockAuthService(ctrl)
+
+	mcPostBody := map[string]string{
+		"nickname": "StepByyyy",
+		"email": "YasaPupkinEzji@top.world",
+		"password": "Widget Adapter",
+	}
+    body, _ := json.Marshal(mcPostBody)
+	r := httptest.NewRequest(http.MethodPost, "/api/v1/auth/login", bytes.NewReader(body))
+
+	r.Header.Set("Content-Type", "application/json")
+
+
+	resSignup := modelsGlobal.User{
+		Email: "YasaPupkinEzji@top.world",
+		Nickname: "StepByyyy",
+		Avatar: "123",
+	}
+
+	resSession := modelsGlobal.Session{
+		ID: "1",
+		User: &resSignup,
+	}
+
+	authService.EXPECT().Signup(r.Context(), &modelsGlobal.User{
+		Email: "YasaPupkinEzji@top.world",
+		Password: "Widget Adapter",
+		Nickname: "StepByyyy",
+	}).Return(resSignup, nil)
+
+	authService.EXPECT().CreateSession(r.Context(), &resSignup).Return(resSession, nil)
+
+	w := httptest.NewRecorder()
+
+	router := mux.NewRouter()
+	loginHandler := NewSingUpHandler(authService)
+	loginHandler.Configure(router, nil)
+
+	loginHandler.Action(w, r)
+
+	// Check code
+	require.Equal(t, http.StatusCreated, w.Code)
+
+	// Check Needed headers
+	require.True(t, len(w.Header().Get("X-Csrf-Token")) > 0)
+
+	// Check body
+	response := w.Result()
+
+	body, err := io.ReadAll(response.Body)
+	require.Nil(t, err, "io.ReadAll must be success")
+
+	err = response.Body.Close()
+	require.Nil(t, err, "Body.Close must be success")
+
+	expectedBody := models.NewUserSignUpResponse(&resSignup)
+
+	var actualBody *models.UserSignupResponse
+
+	err = json.Unmarshal(body, &actualBody)
+	require.Nil(t, err, "json.Unmarshal must be success")
+
+	require.Equal(t, expectedBody, actualBody, "Wrong body")
+}
---
+++ internal/api/http/delivery/handlers/search_test.go
@@ -0,0 +1,108 @@
+package handlers
+
+import (
+	"context"
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/require"
+
+	"go-park-mail-ru/2022_2_BugOverload/internal/api/http/delivery/models"
+	modelsGlobal "go-park-mail-ru/2022_2_BugOverload/internal/models"
+	"go-park-mail-ru/2022_2_BugOverload/internal/pkg/constparams"
+	mockWarehouseClient "go-park-mail-ru/2022_2_BugOverload/internal/warehouse/delivery/grpc/client/mocks"
+)
+
+func TestSearchHandlerPremiere_Action_OK(t *testing.T) {
+	t.Parallel()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	searchService := mockWarehouseClient.NewMockWarehouseService(ctrl)
+
+	r := httptest.NewRequest(http.MethodGet, "/api/v1/search?q=abc", nil) //
+
+	res := modelsGlobal.Search{ //
+		Persons: []modelsGlobal.Person{{
+			ID: 1,
+			Name:         "Шон Коннери",
+			OriginalName: "Sean Connery",
+			Professions:  []string{"актер", "продюсер", "режиссер"},
+			CountFilms:   218,
+			Birthday:     "1930.08.25",
+			Death:        "2020.10.31",
+			Avatar: "12",
+		}},
+
+		Serials: []modelsGlobal.Film{{
+			Name:          "Игра престолов1",
+			ProdDate:      "2013",
+			EndYear:       "2014",
+			ID:            123,
+			Rating:        7.12332,
+			PosterVer:     "123",
+			ProdCountries: []string{"США"},
+			Genres:        []string{"фэнтези", "приключения"},
+			Directors: []modelsGlobal.FilmPerson{{ID: 1, Name: "qqq"}},
+		}},
+
+		Films: []modelsGlobal.Film{{
+			Name:          "Игра престолов2",
+			ProdDate:      "2013",
+			EndYear:       "2014",
+			ID:            123,
+			Rating:        7.12332,
+			PosterVer:     "123",
+			ProdCountries: []string{"США"},
+			Genres:        []string{"фэнтези", "приключения"},
+			Directors: []modelsGlobal.FilmPerson{{ID: 1, Name: "qqq"}},
+		}},
+	}
+
+	oldLogger := logrus.New()
+	logger := logrus.NewEntry(oldLogger)
+
+	ctx := context.WithValue(r.Context(), constparams.LoggerKey, logger)
+
+	r = r.WithContext(ctx)
+
+	searchService.EXPECT().Search(r.Context(), &constparams.SearchParams{ //
+		Query: "abc", //
+	}).Return(res, nil)
+
+	w := httptest.NewRecorder()
+
+	router := mux.NewRouter()
+	handler := NewSearchHandler(searchService) //
+	handler.Configure(router, nil)
+
+	handler.Action(w, r)
+
+	// Check code
+	require.Equal(t, http.StatusOK, w.Code, "Wrong StatusCode")
+
+	// Check body
+	response := w.Result()
+
+	body, err := io.ReadAll(response.Body)
+	require.Nil(t, err, "io.ReadAll must be success")
+
+	err = response.Body.Close()
+	require.Nil(t, err, "Body.Close must be success")
+
+	expectedBody, _ := models.NewSearchResponse(res) //
+
+	var actualBody *models.SearchResponse //
+
+	err = json.Unmarshal(body, &actualBody)
+	require.Nil(t, err, "json.Unmarshal must be success")
+
+	require.Equal(t, &expectedBody, actualBody, "Wrong body")
+}
