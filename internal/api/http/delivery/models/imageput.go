32e9a8e55cc31c4d63fb85da1745dc534be67b73
Andeo1812 <alexanforall@mail.ru>
2022-12-04 03:10:41 +0300

    TP-87f update: http delibery tests, add image and some user tests

--- internal/api/http/delivery/models/imageput.go
+++ internal/api/http/delivery/models/imageput.go
@@ -50,7 +50,6 @@ func (i *PutImageRequest) Bind(r *http.Request) error {
 	if _, errSeek := file.Seek(0, io.SeekStart); errSeek != nil {
 		return errors.ErrBadBodyRequest
 	}
-	http.DetectContentType
 
 	// contentType := http.DetectContentType(fileHeader) more complicated way (ignoring Headers multipart value)
 	contentType := multipartFileHeader.Header.Get("Content-type")
