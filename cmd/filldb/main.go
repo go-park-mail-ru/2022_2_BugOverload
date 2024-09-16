eefa0b76c8559395090ed1ecbbdcda5acc6c071c
Volkov Mike <84146116+Mike5535@users.noreply.github.com>
2024-09-16 15:41:33 +0300

    Update main.go

--- cmd/filldb/main.go
+++ cmd/filldb/main.go
@@ -1,40 +1,40 @@
 package main
 
 import (
-	"flag"
-	"go-park-mail-ru/2022_2_BugOverload/internal/pkg/dev/devpkg"
+        "flag"
+        "go-park-mail-ru/2022_2_BugOverload/internal/pkg/dev/devpkg"
 
-	"github.com/BurntSushi/toml"
-	"github.com/sirupsen/logrus"
+        "github.com/BurntSushi/toml"
+        "github.com/sirupsen/logrus"
 
-	"go-park-mail-ru/2022_2_BugOverload/internal/pkg/dev/fillerdb"
+        "go-park-mail-ru/2022_2_BugOverload/internal/pkg/dev/fillerdb"
 )
 
 func main() {
-	var configPath string
-	var dataPath string
+        var configPath string
+        var dataPath string
 
-	flag.StringVar(&configPath, "config-path", "cmd/filldb/configs/debug.toml", "path to config file")
-	flag.StringVar(&dataPath, "data-path", "test/newdata", "path to data files")
+        flag.StringVar(&configPath, "config-path", "cmd/filldb/configs/debug.toml", "path to config file")
+        flag.StringVar(&dataPath, "data-path", "test/newdata", "path to data files")
 
-	flag.Parse()
+        flag.Parse()
 
-	config := devpkg.NewConfig()
+        config := devpkg.NewConfig()
 
-	_, err := toml.DecodeFile(configPath, config)
-	if err != nil {
-		logrus.Fatal(err)
-	}
+        _, err := toml.DecodeFile(configPath, config)
+        if err != nil {
+                logrus.Fatal(err)
+        }
 
-	filler, err := fillerdb.NewDBFiller(dataPath, config)
-	if err != nil {
-		logrus.Fatalf("FAILED  [%s]", err)
-	}
+        filler, err := fillerdb.NewDBFiller(dataPath, config)
+        if err != nil {
+                logrus.Fatalf("FAILED  [%s]", err)
+        }
 
-	err = filler.Action()
-	if err != nil {
-		logrus.Fatalf("FAILED [%s]", err)
-	}
+        err = filler.Action()
+        if err != nil {
+                logrus.Fatalf("FAILED [%s]", err)
+        }
 
-	logrus.Info("SUCCESS")
+        logrus.Info("SUCCESS")
 }
